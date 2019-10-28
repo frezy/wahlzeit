733f89903fbbdcd5ba99879793babad5dc940456
frezy <frezy@frezy.at>
2019-10-28 14:26:48 +0100

    Implemented double compare with PRECISION Added getter/setter for Location and Coordinate Implemented hashCode for Location and Coordinate

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -13,6 +13,30 @@ public class Coordinate {
         this.z = z;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
     /**
      * Calculate the euclidean distance between the actual Coordinate-object and another.
      * @param coordinate The other Coordiante-object.
@@ -29,16 +53,30 @@ public class Coordinate {
      */
     public boolean isEqual(Coordinate coordinate) {
         if (this == coordinate) return true;
-        return Double.compare(coordinate.x, x) == 0 &&
-                Double.compare(coordinate.y, y) == 0 &&
-                Double.compare(coordinate.z, z) == 0;
+
+        return compareDouble(coordinate.x, x) &&
+                compareDouble(coordinate.y, y) &&
+                compareDouble(coordinate.z, z);
+    }
+
+    private static final double PRECISION = 1E-5;
+    private static boolean compareDouble(double x, double y) {
+        if(Double.isNaN(x) || Double.isNaN(y)) return false;
+
+        return Math.abs(x - y) < PRECISION;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        
         Coordinate coordinate = (Coordinate) o;
         return this.isEqual(coordinate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
     }
 }
--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -8,11 +8,29 @@ import java.util.Objects;
 public class Location {
     public Coordinate coordinate;
 
+    public Location(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         Location location = (Location) o;
         return coordinate.equals(location.coordinate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinate);
     }
 }
